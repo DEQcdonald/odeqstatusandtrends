4f8a0cc5406146a66ac107a45e134c536c7b5938
donco <colin.donald@outlook.com>
2019-02-28 13:57:39 -0800

    Optional GetData NWIS query
    Made NWIS query optional

--- R/GetData.R
+++ R/GetData.R
@@ -1,7 +1,7 @@
 #' Get data for water quality status and trends analysis
-#' 
-#' Queries the Oregon DEQ AWQMS database for water quality monitoring data fitting the given parameters. Requires permissions to the VW_AWQMS_Results 
-#' view in the AWQMS repository. This query is specific to the methods used in status and trends reporting. For a more comprehensive AWQMS query, see 
+#'
+#' Queries the Oregon DEQ AWQMS database for water quality monitoring data fitting the given parameters. Requires permissions to the VW_AWQMS_Results
+#' view in the AWQMS repository. This query is specific to the methods used in status and trends reporting. For a more comprehensive AWQMS query, see
 #' the AWQMS package by Travis Pritchard.
 #' @param parameters A list of parameters to include in the query
 #' @param stations_AWQMS Stations dataframe from get_stations_AWQMS()
@@ -12,74 +12,76 @@
 #' @return A dataframe of all available data within AWQMS that fit the query.
 #' @export
 #' @examples
-#' GetData(parameters = c("parameter1", "parameter2"), 
+#' GetData(parameters = c("parameter1", "parameter2"),
 #' stations_AWQMS = result-of-get_stations_AWQMS(),
 #' stations_NWIS = result-of-get_stations_NWIS(),
-#' start.date = "2010-01-01", end.date = "2019-01-01", 
+#' start.date = "2010-01-01", end.date = "2019-01-01",
 #' awqms.channel.name = "AWQMS")
 
-GetData <- function(parameters = NULL, stations_AWQMS, stations_NWIS, start.date, end.date, awqms.channel.name = "AWQMS") {
-  
+GetData <- function(parameters = NULL, stations_AWQMS, start.date, end.date, query_nwis = FALSE, stations_NWIS, awqms.channel.name = "AWQMS") {
+
   # Convert characteristic names
   AWQMS.parms <- AWQMS_Char_Names(parameters)
-  
+
   #### Define sample media to query ####
   sample.media <- 'Water'
-  
+
   print(paste('Querying the AWQMS database for data at', length(stations_AWQMS$MLocID), 'stations related to:', paste(parameters, collapse = ", ")))
-  
+
   s.time <- Sys.time()
-  data_AWQMS <- AWQMSdata::AWQMS_Data(startdate = start.date, 
-                                      enddate = end.date, 
-                                      char = AWQMS.parms, 
+  data_AWQMS <- AWQMSdata::AWQMS_Data(startdate = start.date,
+                                      enddate = end.date,
+                                      char = AWQMS.parms,
                                       media = sample.media,
-                                      crit_codes = TRUE, 
+                                      crit_codes = TRUE,
                                       station = stations_AWQMS$MLocID)
   e.time <- Sys.time()
   print(paste("This query took approximately", difftime(e.time, s.time, units = "secs"), "seconds."))
-  
+
   data_AWQMS <- data_AWQMS %>% filter(MonLocType %in% c("River/Stream", "Lake", "Other-Surface Water", ""))
-  
+
   data_AWQMS <- merge(data_AWQMS, stations_AWQMS[, c("MLocID", "Datum")], by="MLocID", all.x = TRUE, all.y = FALSE)
-  
-  usgs_stations <- unique(stations_NWIS$site_no)
-  if("Temperature, water" %in% AWQMS.parms) {
-    usgs_parms <- c("00010")
+
+  if(query_nwis){
+    usgs_stations <- unique(stations_NWIS$site_no)
+    if("Temperature, water" %in% AWQMS.parms) {
+      usgs_parms <- c("00010")
+    }
+    if("Dissolved oxygen (DO)" %in% AWQMS.parms) {
+      usgs_parms <- c(usgs_parms, "00300", "00301")
+    }
+
+    print(paste('Querying the NWIS database for data at', length(usgs_stations), 'stations related to:',
+                paste(parameterCdFile[parameterCdFile$parameter_cd %in% usgs_parms,"parameter_nm"], collapse = ", ")))
+
+    s.time <- Sys.time()
+    data_NWIS <- dataRetrieval::readNWISdata(sites = usgs_stations,
+                                             startDt = start.date,
+                                             endDt = end.date,
+                                             siteType = c("ES", "LK", "SP", "ST", "ST-CA", "ST-DCH", "ST-TS", "WE"),
+                                             parameterCd = usgs_parms)
+    e.time <- Sys.time()
+    print(paste("This query took approximately", difftime(e.time, s.time, units = "secs"), "seconds."))
+
+    data_NWIS <- merge(data_NWIS, attributes(data_NWIS)$siteInfo, by = c("site_no", "agency_cd"), all.x = TRUE, all.y = FALSE)
+    data_NWIS <- dplyr::rename(data_NWIS, Result_Numeric = X_00010_00001,
+                               StationDes = station_nm,  OrganizationID = agency_cd, MLocID = site_no, SampleStartDate = dateTime,
+                               SampleStartTZ = tz_cd, Lat_DD = dec_lat_va, Long_DD = dec_lon_va, Datum = srs, MonLocType = siteTypeCd,
+                               HUC8 = hucCd)
+    data_NWIS <- dplyr::mutate(data_NWIS, Statistical_Base = "Maximum", Tesult_Type = "Calculated", Result_Unit = "deg C",
+                               Char_Name = "Temperature, water", Activity_Type = "NWIS", SampleMedia = "Water",
+                               SampleSubmedia = "Surface Water", Unit_UID = 246)
+    data_NWIS$SampleStartDate <- as.character(data_NWIS$SampleStartDate)
+
+    data_combined <- bind_rows(data_AWQMS, data_NWIS)
   }
-  if("Dissolved oxygen (DO)" %in% AWQMS.parms) {
-    usgs_parms <- c(usgs_parms, "00300", "00301")
-  }
-  
-  print(paste('Querying the NWIS database for data at', length(usgs_stations), 'stations related to:', 
-              paste(parameterCdFile[parameterCdFile$parameter_cd %in% usgs_parms,"parameter_nm"], collapse = ", ")))
-  
-  s.time <- Sys.time()
-  data_NWIS <- dataRetrieval::readNWISdata(sites = usgs_stations,
-                                           startDt = start.date,
-                                           endDt = end.date,
-                                           siteType = c("ES", "LK", "SP", "ST", "ST-CA", "ST-DCH", "ST-TS", "WE"),
-                                           parameterCd = usgs_parms)
-  e.time <- Sys.time()
-  print(paste("This query took approximately", difftime(e.time, s.time, units = "secs"), "seconds."))
-  
-  data_NWIS <- merge(data_NWIS, attributes(data_NWIS)$siteInfo, by = c("site_no", "agency_cd"), all.x = TRUE, all.y = FALSE)
-  data_NWIS <- dplyr::rename(data_NWIS, Result_Numeric = X_00010_00001,
-                             StationDes = station_nm,  OrganizationID = agency_cd, MLocID = site_no, SampleStartDate = dateTime,
-                             SampleStartTZ = tz_cd, Lat_DD = dec_lat_va, Long_DD = dec_lon_va, Datum = srs, MonLocType = siteTypeCd,
-                             HUC8 = hucCd)
-  data_NWIS <- dplyr::mutate(data_NWIS, Statistical_Base = "Maximum", Tesult_Type = "Calculated", Result_Unit = "deg C",
-                             Char_Name = "Temperature, water", Activity_Type = "NWIS", SampleMedia = "Water", 
-                             SampleSubmedia = "Surface Water", Unit_UID = 246)
-  data_NWIS$SampleStartDate <- as.character(data_NWIS$SampleStartDate)
-  
-  data_combined <- bind_rows(data_AWQMS, data_NWIS)
-  
+
   # #### connect to element and get data (must set up ODBC connection first) ####
   # channel <- odbcConnect(awqms.channel.name)
   # table <- "VW_AWQMS_Results"
-  # 
+  #
   # site.type = "'River/Stream', 'Lake', 'Other-Surface Water', 'Reservoir'"
-  # 
+  #
   # #### Create SQL query ####
   # myQuery <- paste0("SELECT * FROM VW_AWQMS_Results WHERE MLocID IN ('", paste(stations, collapse = "', '"),
   #                   "') AND Char_Name IN ('", paste(AWQMS.parms, collapse = "', '"),
@@ -87,14 +89,14 @@ GetData <- function(parameters = NULL, stations_AWQMS, stations_NWIS, start.date
   #                   "' AND SampleMedia='", sample.media,
   #                   "' AND MonLocType IN (", site.type, ")"
   # )
-  # 
+  #
   # print(paste('Querying', length(stations), 'stations from AWQMS for water quality data related to:', paste(parameters, collapse = ", ")))
-  # 
+  #
   # #### Pass the SQL query to AWQMS ####
   # sTime <- Sys.time()
   # AWQMS.data <- sqlQuery(channel, myQuery, errors = FALSE)
   # eTime <- Sys.time()
   # print(paste("This query took approximately", difftime(eTime, sTime, units = "secs"), "seconds."))
-  
+
   return(data.AWQMS)
 }
--- R/plot_temperature.R
+++ R/plot_temperature.R
@@ -17,18 +17,21 @@ plot_temperature <- function(data, seaKen, station){
     y_median <- median(plot_data$Result_Numeric, na.rm = TRUE)
     sk_min <- y_median - x_delta*slope/365.25
     sk_max <- y_median + x_delta*slope/365.25
-  }    
-  
+  }
+
   p <- ggplot(plot_data) +
-    geom_point(aes(x=sample_datetime, y=Result_Numeric, color=exceed, linetype = exceed, shape=exceed))
+    geom_point(aes(x=sample_datetime, y=Result_Numeric, color=exceed, linetype = exceed, shape=exceed)) +
+    ggtitle(paste(station)) +
+    ylab("Temperature (degrees C)") +
+    xlab("Datetime")
   if(station %in% seaKen$MLocID){
     p <- p + geom_segment(aes(x=x_min, xend=x_max, y=sk_min, yend=sk_max, color = "Trend", linetype = "Trend", shape = "Trend"))
   }
-    
-  p <- p + 
+
+  p <- p +
     scale_color_manual(values = c('TRUE' = 'red', 'FALSE' = 'black', "Trend" = 'blue')) +
     scale_linetype_manual(values = c('TRUE' = 0, 'FALSE' = 0, "Trend" = 1)) +
     scale_shape_manual(values = c('TRUE' = 16, 'FALSE' = 16, "Trend" = 20))
-  
+
   return(p)
 }
