f46f6e7f8b9a47b6f1e52f8e4fd51f88c335dfcf
donco <colin.donald@outlook.com>
2019-05-13 09:52:32 -0700

    Accounted for TP names in AWQMS

--- R/AWQMS_Char_Names.R
+++ R/AWQMS_Char_Names.R
@@ -1,50 +1,50 @@
 #' AWQMS Characteristic Names
-#' 
+#'
 #' Converts input parameters to AWQMS characteristics names
 #' @param parameters A list of parameters to convert.
 #' @return A list of relevant AWQMS parameters
 #' @export
-#' @examples 
+#' @examples
 #' AWQMS_Char_Names(parameters = c("Temperature", "Bacteria", "TSS"))
 
 AWQMS_Char_Names <- function(parameters){
-  
+
   if(is.null(parameters)){return(NULL)}
-  
+
   #### Expand bacteria to include fecal and enterococcus ####
   if(any(parameters == 'Bacteria')) {
     parameters <- c(parameters, c('Ecoli','Fecal coliform','Enterococcus'))
     parameters <- unique(parameters[parameters != "Bacteria"])
   }
-  
+
   #### Lookup table for parameters and their AWQMS database name ####
   parms.lookup <- data.frame(General = c("tss",
                                          "temperature",
                                          "tp",
                                          "total phosphorus",
-                                         "ph", 
-                                         "fecal coliform", 
+                                         "ph",
+                                         "fecal coliform",
                                          "ecoli",
                                          "e. coli",
                                          "e.coli",
-                                         "enterococcus", 
+                                         "enterococcus",
                                          "do",
-                                         "dissolved oxygen"), 
-                             AWQMS.Name = c("Total suspended solids", 
-                                            "Temperature, water", 
-                                            "Phosphorus",
-                                            "Phosphorus",
+                                         "dissolved oxygen"),
+                             AWQMS.Name = c("Total suspended solids",
+                                            "Temperature, water",
+                                            "Phosphate-phosphorus",
+                                            "Phosphate-phosphorus",
                                             "pH",
                                             "Fecal Coliform",
                                             "Escherichia coli",
                                             "Escherichia coli",
                                             "Escherichia coli",
-                                            "Enterococcus", 
-                                            "Dissolved oxygen (DO)", 
+                                            "Enterococcus",
+                                            "Dissolved oxygen (DO)",
                                             "Dissolved oxygen (DO)"),
                              stringsAsFactors = FALSE
   )
-  
+
   #### Convert parameters to AWQMS parameter names for query ####
   AWQMS.parms <- parms.lookup[parms.lookup$General %in% tolower(parameters),"AWQMS.Name"]
 
