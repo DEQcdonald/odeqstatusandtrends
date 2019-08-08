b3ed2805ae43321d65d0ad1a455319d7e32d11cd
donco <colin.donald@outlook.com>
2019-08-08 13:22:22 -0700

    Updated to include AU_Name

--- R/parameter_summary.R
+++ R/parameter_summary.R
@@ -11,11 +11,11 @@
 parameter_summary_by_station <- function(status, sea_ken, stations){
   st_stations <- unique(c(status$MLocID, sea_ken$MLocID))
   st_stations_info <- stations %>% dplyr::filter(MLocID %in% st_stations) %>%
-    dplyr::select(AU_ID, GNIS_Name, MLocID, StationDes, Lat_DD, Long_DD, HUC8, HUC8_Name)
+    dplyr::select(AU_Name, AU_ID, MLocID, StationDes, Lat_DD, Long_DD, HUC8, HUC8_Name)
 
   param_sum <- merge(st_stations_info, status, by = "MLocID")
   param_sum <- merge(param_sum, sea_ken, by = c("MLocID", "Char_Name"), all.x = TRUE)
-  param_sum <- dplyr::select(param_sum, AU_ID, GNIS_Name, Char_Name, MLocID, StationDes,
+  param_sum <- dplyr::select(param_sum, AU_Name, AU_ID, Char_Name, MLocID, StationDes,
                              rev(colnames(status)[3:length(colnames(status))]), trend, Lat_DD, Long_DD, HUC8, HUC8_Name)
   param_sum <- param_sum[order(param_sum[,1], param_sum[,3], param_sum[,4]),]
   param_sum[is.na(param_sum$trend), "trend"] <- "Insufficient Data"
@@ -35,16 +35,16 @@ parameter_summary_by_station <- function(status, sea_ken, stations){
 parameter_summary_by_au <- function(status, sea_ken, stations){
   st_stations <- unique(c(status$MLocID, sea_ken$MLocID))
   st_stations_info <- stations %>% dplyr::filter(MLocID %in% st_stations) %>%
-    dplyr::select(AU_ID, GNIS_Name, MLocID, StationDes, Lat_DD, Long_DD, HUC8, HUC8_Name)
+    dplyr::select(AU_ID, AU_Name, MLocID, StationDes, Lat_DD, Long_DD, HUC8, HUC8_Name)
 
   param_sum <- merge(st_stations_info, status, by = "MLocID")
   param_sum <- merge(param_sum, sea_ken, by = c("MLocID", "Char_Name"), all.x = TRUE)
-  param_sum <- dplyr::select(param_sum, AU_ID, GNIS_Name, Char_Name, MLocID, StationDes,
+  param_sum <- dplyr::select(param_sum, AU_ID, AU_Name, Char_Name, MLocID, StationDes,
                              rev(colnames(status)[3:length(colnames(status))]), trend, Lat_DD, Long_DD, HUC8, HUC8_Name)
   param_sum <- param_sum[order(param_sum[,1], param_sum[,3], param_sum[,4]),]
   param_sum[is.na(param_sum$trend), "trend"] <- "Insufficient Data"
 
-  assess_sum <- param_sum %>% dplyr::select(-GNIS_Name, -MLocID, -StationDes, -trend, -Lat_DD, -Long_DD, -HUC8, -HUC8_Name) %>%
+  assess_sum <- param_sum %>% dplyr::select(-MLocID, -StationDes, -trend, -Lat_DD, -Long_DD, -HUC8, -HUC8_Name) %>%
     dplyr::group_by(AU_ID, Char_Name) %>%
     dplyr::summarise_all(function(x){
       y <- if_else(any(x == "Not Attaining"),
--- R/st_report.R
+++ R/st_report.R
@@ -10,13 +10,16 @@
 #' @examples
 #' st_report(basin = "Willamette", param_summary = param_sum_Willamette, out_path = ".../your-output-path")
 
-st_report <- function(basin, param_summary, file_name, out_path){
+st_report <- function(basin, param_summary, file_name, out_path, complete_years, hucs){
   out_path <- gsub("\\\\", "/", out_path)
+
   rmarkdown::render(input = "N:/Status_and_Trend_Reports/Report_Files/basin.Rmd",
-                  params = list(
-                    basin = basin,
-                    param_sum = param_summary),
-                  output_file = file_name,
-                  output_dir = out_path,
-                  envir = parent.frame())
+                    params = list(
+                      basin = basin,
+                      param_sum = param_summary,
+                      complete_years = complete_years,
+                      hucs = hucs),
+                    output_file = file_name,
+                    output_dir = out_path,
+                    envir = parent.frame())
 }
