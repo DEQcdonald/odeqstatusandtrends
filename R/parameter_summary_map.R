368588d19496cd75768663e594c23566202a46d7
donco <colin.donald@outlook.com>
2019-08-12 14:36:39 -0700

    Update parameter_summary_map.R
    Fixed trend in popup tables

--- R/parameter_summary_map.R
+++ R/parameter_summary_map.R
@@ -112,7 +112,7 @@ parameter_summary_map <- function(param_summary, au_param_summary, area){
     data <- param_summary %>% dplyr::rename(Parameter = Char_Name, Station_ID = MLocID, Station_Description = StationDes)
 
     if(!is.null(station)){
-      data <- filter(data[, c(4, 3, grep("status", colnames(param_summary)), "trend")],
+      data <- filter(data[, c(4, 3, grep("status|trend", colnames(param_summary)))],
                      Station_ID == station, Parameter == param)
       # %>%
         # dplyr::select(-Parameter, -Station_ID)
@@ -128,7 +128,7 @@ parameter_summary_map <- function(param_summary, au_param_summary, area){
 
     }
     if(!is.null(AU)){
-      data <- dplyr::filter(data[, c(1, 4, 3, grep("status", colnames(param_summary)))],
+      data <- dplyr::filter(data[, c(1, 4, 3, grep("status|trend", colnames(param_summary)))],
                      AU_ID == AU, Parameter == param) %>%
         dplyr::select(-AU_ID)
 
