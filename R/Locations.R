9d1921b95a1a6e97ef309810479b1d942c319f0c
donco <colin.donald@outlook.com>
2019-09-04 12:13:01 -0700

    Added MonLocType

--- R/Locations.R
+++ R/Locations.R
@@ -48,8 +48,8 @@ get_stations_AWQMS <- function(polygon, exclude.tribal.lands = TRUE, stations.ch
     print("The following stations have missing AU IDs and were removed...")
     missing_au <- stations[is.na(stations$AU_ID),]
     stations <- stations %>% dplyr::filter(!MLocID %in% missing_au$MLocID)
-    print(missing_au[,c("OrgID", "MLocID", "AU_ID", "Lat_DD", "Long_DD")])
-    attr(stations, 'missing_AUs') <- missing_au[,c("OrgID", "MLocID", "AU_ID", "Lat_DD", "Long_DD")]
+    print(missing_au[,c("OrgID", "MLocID", "MonLocType", "AU_ID", "Lat_DD", "Long_DD")])
+    attr(stations, 'missing_AUs') <- missing_au[,c("OrgID", "MLocID", "MonLocType", "AU_ID", "Lat_DD", "Long_DD")]
   }
 
   if(any(stations$AU_ID == "99")){
