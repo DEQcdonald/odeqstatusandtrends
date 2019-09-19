1f57a59785a62b057bdf21becc80b3c46fb5ed38
donco <colin.donald@outlook.com>
2019-09-19 08:14:02 -0700

    Update st_report.R

--- R/st_report.R
+++ R/st_report.R
@@ -3,6 +3,7 @@
 #' Creates a Status and Trends Report with the given parameter summary table, Basin name, and output directory.
 #' @param basin The name of the basin for reporting
 #' @param param_summary A parameter summary table as returned from odeqstatusandtrends::parameter_summary().
+#' @param format The output format for the rmarkdown document
 #' @param file_name The name of the output .html file
 #' @param out_path The output path
 #' @param complete_years A list of complete years within analysis
@@ -12,7 +13,7 @@
 #' @examples
 #' st_report(basin = "Willamette", param_summary = param_sum_Willamette, out_path = ".../your-output-path")
 
-st_report <- function(basin, param_summary, file_name, out_path, complete_years, hucs){
+st_report <- function(basin, param_summary, format = "html_document", file_name, out_path, complete_years, hucs){
   out_path <- gsub("\\\\", "/", out_path)
 
   rmarkdown::render(input = "N:/Status_and_Trend_Reports/Report_Files/basin.Rmd",
@@ -21,6 +22,7 @@ st_report <- function(basin, param_summary, file_name, out_path, complete_years,
                       param_sum = param_summary,
                       complete_years = complete_years,
                       hucs = hucs),
+                    output_format = format,
                     output_file = file_name,
                     output_dir = out_path,
                     envir = parent.frame())
