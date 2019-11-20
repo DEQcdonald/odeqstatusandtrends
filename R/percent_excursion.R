a313b010fd36f869378d42e871e46b5395082fb5
rmichie <michie.ryan@deq.state.or.us>
2019-11-20 13:43:02 -0800

    Add exursion summary stats
    Excursion summary statistics include number of excursions, percent excursion, and the max, median, and minimum for results that contributed to an excursion.
    Updated documentation.

--- R/percent_excursion.R
+++ R/percent_excursion.R
@@ -1,17 +1,22 @@
-#' Determine percent excursion for status years
+#' Calculate excursion statistics for status years
 #'
-#' Creates a dataframe with stations, the number of years with data within
-#' the status years, and the percent excursion of the results by parameter.
+#' Creates a dataframe with excursion statistics by station, parmameter, and status year bin.
+#' Excursion statistics include number of excursions, percent excursion, and the max, median, and minimum for results
+#' that contributed to an excursion.
+#'
 #' @param data Dataframe to determine status from.
 #' @param year_range Years from which to determine status. If null, the year range for data provided is used.
-#' @param status_period Number of whole years to include in a status analysis.
 #' The range of years in status years will be binned by this number
-#' @return Dataframe of stations with sufficient data
+#' @return Dataframe of stations
 #' @export
 #' @examples
-#' percent_excursion(data = data.frame, year_range = c(start_year, end_year), status_period = 5)
+#' percent_excursion(data = data.frame, year_range = c(start_year, end_year))
 
-percent_excursion <- function(data, year_range = NULL, status_period = 4) {
+
+percent_excursion <- function(data, year_range = NULL) {
+
+  # This assumes a 20 year period
+  status_period <- 4
 
   if("Phosphate-phosphorus" %in% unique(data$Char_Name)){
     if("tp_year" %in% colnames(data)){
@@ -45,16 +50,26 @@ percent_excursion <- function(data, year_range = NULL, status_period = 4) {
 
   if(any(unique(data$year) %in% years)){
 
-    per_excursion_df <- data %>%
+    excursion_stat_df1 <- data %>%
+      dplyr::filter(year %in% years,
+                    !(BacteriaCode == 3 & Char_Name == "Fecal Coliform")) %>%
+      dplyr::filter(excursion_cen==1) %>%
+      dplyr::group_by(MLocID, Char_Name, bin) %>%
+      dplyr::summarise(excursion_max = max(Result_Numeric, na.rm = TRUE),
+                       excursion_median = min(Result_Numeric, na.rm = TRUE),
+                       excursion_min = min(Result_Numeric, na.rm = TRUE)) %>%
+      dplyr::ungroup()
+
+    excursion_stat_df2 <- data %>%
       dplyr::filter(year %in% years,
                     !(BacteriaCode == 3 & Char_Name == "Fecal Coliform")) %>%
       dplyr::group_by(MLocID, Char_Name, bin) %>%
       dplyr::summarise(results_n = n(),
-                       excursions = sum(excursion_cen, na.rm = TRUE),
-                       percent_excursion = round(excursions/results_n*100,0)) %>%
+                       excursions_n = sum(excursion_cen, na.rm = TRUE),
+                       percent_excursion = round(excursions_n/results_n*100,0)) %>%
       dplyr::ungroup() %>%
-      dplyr::select(-excursions) %>%
-      tidyr::pivot_wider(names_from=bin, values_from=c(percent_excursion, results_n))
+      dplyr::left_join(by=c("MLocID", "Char_Name", "bin"), y=excursion_stat_df1) %>%
+      tidyr::pivot_wider(names_from=bin, values_from=c(percent_excursion, results_n, excursions_n, exursion_max, exursion_median, exursion_min))
 
     if(any(data$year %in% years & data$BacteriaCode == 3 & data$Char_Name == "Fecal Coliform")){
       shell_per_excursion <- data %>%
@@ -64,15 +79,15 @@ percent_excursion <- function(data, year_range = NULL, status_period = 4) {
         dplyr::group_by(MLocID, Char_Name, bin) %>%
         dplyr::summarise(results_n = n(),
                          median = if_else(results_n >= 5, median(Result_cen), NaN),
-                         excursions = sum(perc_exceed),
+                         excursions_n = sum(perc_exceed),
                          bact_crit_percent = first(bact_crit_percent),
                          bact_crit_ss = first(bact_crit_ss),
                          n_years = length(unique(year)),
                          excursion = dplyr::if_else((!is.na(median) & median > bact_crit_ss),
                                                     1,
-                                                    if_else(results_n >= 10 & excursions/results_n > 0.10,
+                                                    if_else(results_n >= 10 & excursions_n/results_n > 0.10,
                                                             1,
-                                                            if_else(results_n >= 5 & results_n <= 9 & excursions >= 1,
+                                                            if_else(results_n >= 5 & results_n <= 9 & excursions_n >= 1,
                                                                     1, 0)
                                                     )),
                          percent_excursion = ifelse(!is.na(excursion),
@@ -80,28 +95,28 @@ percent_excursion <- function(data, year_range = NULL, status_period = 4) {
                                                 NaN)
         ) %>%
         dplyr::ungroup() %>%
-        dplyr::select(-excursions) %>%
+        dplyr::select(-excursions_n) %>%
         tidyr::pivot_wider(names_from=bin, values_from=c(percent_excursion, results_n))
 
       if(nrow(shell_per_excursion) >0){
-        per_excursion_df <- dplyr::bind_rows(per_excursion_df, shell_per_excursion)
+        excursion_stat_df2 <- dplyr::bind_rows(excursion_stat_df2, shell_per_excursion)
       }
 
     }
 
-    cols <- c(colnames(per_excursion_df[,c(1,2, rev(3:length(colnames(per_excursion_df))))]))
+    cols <- c(colnames(excursion_stat_df2[,c(1,2, rev(3:length(colnames(excursion_stat_df2))))]))
 
-    for(i in cols[!cols %in% colnames(per_excursion_df)]){
-      per_excursion_df[,i] <- NaN
+    for(i in cols[!cols %in% colnames(excursion_stat_df2)]){
+      excursion_stat_df2[,i] <- NaN
     }
 
-    per_excursion_df <- per_excursion_df[,cols]
+    excursion_stat_df2 <- excursion_stat_df2[,cols]
 
-    print(paste("Data should be sufficient for percent exceedance calculations at ", NROW(per_excursion_df), "different stations."))
+    print(paste("Data should be sufficient for percent exceedance calculations at ", NROW(excursion_stat_df2), "different stations."))
 
   } else {
-    per_excursion_df <- "No stations meet criteria"
-    return(per_excursion_df)
+    excursion_stat_df2 <- "No stations meet criteria"
+    return(excursion_stat_df2)
   }
-  return(per_excursion_df[,c(1,2,rev(3:length(colnames(per_excursion_df))))])
+  return(excursion_stat_df2[,c(1,2,rev(3:length(colnames(excursion_stat_df2))))])
 }
