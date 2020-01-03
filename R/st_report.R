668604923adf499def476103005a764ab1ee25c7
rmichie <michie.ryan@deq.state.or.us>
2020-01-03 13:09:56 -0800

    Adding rmd_dir argument
    Adding an argument to explcityly identify where the Rmd files are located.

--- R/st_report.R
+++ R/st_report.R
@@ -4,6 +4,7 @@
 #' @param format The output format for the rmarkdown document
 #' @param table_format The format for kable tables. "pandoc" is the default. See knitr::kable() for more options
 #' @param file_name The name of the output .html file
+#' @param rmd_dir The path to the directory where the status and trend Rmarkdown files are saved.
 #' @param out_path The output path
 #' @return A report located in the out_path
 #' @export
@@ -12,12 +13,13 @@
 #' out_path = "N:/Status_and_Trend_Reports/2019/Statewide Report")
 
 st_report <- function(format = "word_document", table_format = "pandoc", file_name = "Oregon_SandT_report",
+                      rmd_dir = "N:/Status_and_Trend_Reports/Report_Files/st_report_files",
                       out_dir = "N:/Status_and_Trend_Reports/2019/Statewide Report"){
 
   table_format <<- table_format
   out_dir <<- out_dir
 
-  rmarkdown::render(input = "N:/Status_and_Trend_Reports/Report_Files/st_report_files/state_summary.Rmd",
+  rmarkdown::render(input = paste0(rmd_dir, "/state_summary.Rmd"),
                     output_format = format,
                     output_file = file_name,
                     output_dir = out_dir,
