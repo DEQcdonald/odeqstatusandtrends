bcd53e55b3c5dbb2f4ba5befaf19cfce1bc40ed3
donco <colin.donald@outlook.com>
2020-08-07 19:08:01 -0700

    Typo

--- R/parameter_summary_map.R
+++ R/parameter_summary_map.R
@@ -52,7 +52,7 @@ parameter_summary_map <- function(param_summary, au_param_summary, area, proj_di
   assessment_units_bodies <- sf::st_read(
     dsn = "//deqhq1/GISLIBRARY/Base_Data/DEQ_Data/Water_Quality/WQ_2018_IntegratedReport/WQ_Assessment_2018_20.gdb",
     layer = "AssessmentUnit_Waterbodies",
-    query = paste0("SELECT * FROM AssessmentUnit_Waterbodies WHERE AU_ID IN ('",
+    query = paste0("SELECT * FROM AssessmentUnit_OR_Waterbodies WHERE AU_ID IN ('",
                    paste(unique(param_summary$AU_ID), collapse = "', '"), "')"), stringsAsFactors = FALSE
   )
 
@@ -171,7 +171,7 @@ parameter_summary_map <- function(param_summary, au_param_summary, area, proj_di
   wql_bodies <- sf::st_read(
     dsn = "//deqhq1/GISLIBRARY/Base_Data/DEQ_Data/Water_Quality/WQ_2018_IntegratedReport/WQ_Assessment_2018_20.gdb",
     layer = "Impaired_Pollutant_Waterbodies",
-    query = paste0("SELECT * FROM Impaired_Pollutant_Watershed WHERE HUC12 IN ('",
+    query = paste0("SELECT * FROM Impaired_Pollutant_Waterbodies WHERE HUC12 IN ('",
                    paste(huc_12s, collapse = "', '"), "')"),
     stringsAsFactors = FALSE
   )
